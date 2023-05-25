dfdcfd35699dbce5ebb38d31aac8597a2581a1ae
agaaaa24 <agaaaa24@gmail.com>
2023-05-25 16:48:25 +0200

    First attempt to convert new data to stus
    - sumsum
    - xsum

---
+++ load_data.py
@@ -0,0 +1,32 @@
+import json
+
+#file_path = 'eval_interface/src/data/stus_rose(Source)/samsum_stus.txt'
+#summary_path = 'eval_interface/src/data/stus_rose(Source)/samsum.target'
+#output_file_path = 'eval_interface/src/data/samsum/samsum-stus.json'
+file_path = 'eval_interface/src/data/stus_rose(Source)/cnndm.target'
+summary_path = 'eval_interface/src/data/stus_rose(Source)/cnndm.target'
+output_file_path = 'eval_interface/src/data/cnndm/cnndm_test_stus.json'
+#file_path = 'eval_interface/src/data/stus_rose(Source)/xsum_stus.txt'  
+#summary_path = 'eval_interface/src/data/stus_rose(Source)/xsum.target'
+#output_file_path = 'eval_interface/src/data/xsum/xsum-stus.json'
+data = []
+
+with open(file_path, 'r') as file:
+    lines = file.readlines()
+
+with open(summary_path, 'r') as summary_file:
+    summaries = summary_file.readlines()
+
+for i, line in enumerate(lines):
+    entry = {
+        "instance_id": f"sumsum-{i}",
+        "summary": summaries[i].strip(),
+        "stus": [text.strip() for text in line.split('\t')]
+    }
+    data.append(entry)
+
+# Save data as JSON to a new file
+with open(output_file_path, 'w') as output_file:
+    json.dump(data, output_file, indent=2)
+
+print(f"Data saved to {output_file_path}.")
